1057ed6c953f0b96313ac402c278a42c319ff833
José David Sánchez <49158320+Arzoid29@users.noreply.github.com>
2022-07-11 16:34:48 -0400

    Adding the first part of a CRUD with Firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { collection, getDocs } from "firebase/firestore";
+import { useState, useEffect } from "react";
+import { db } from "./firebase-config";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [users, setUsers] = useState([]);
+  const usersCollection = collection(db, "users");
+  useEffect(() => {
+    const getUsers = async () => {
+    const data  = await getDocs(usersCollection);
+    }
+    getUsers();
+  }, []);
+  return <div className="App"></div>;
 }
 
 export default App;
