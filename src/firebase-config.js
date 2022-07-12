d9e365f77552973f54c435644db14c011784d192
José David Sánchez <49158320+Arzoid29@users.noreply.github.com>
2022-07-12 00:07:06 -0400

    Doing the rest of the functions

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,14 +1,13 @@
 import { initializeApp } from "firebase/app";
-import {getFirestore} from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCXILK-A89FVh2V_85o9ojqrwaR6bZo9sU",
-    authDomain: "crud-6671c.firebaseapp.com",
-    projectId: "crud-6671c",
-    storageBucket: "crud-6671c.appspot.com",
-    messagingSenderId: "1067770999919",
-    appId: "1:1067770999919:web:ede3a4873f64ae4f7c099a"
-  };
-
-const app = initializeApp(firebaseConfig);  // Initialize Firebase app
+  apiKey: "AIzaSyCXILK-A89FVh2V_85o9ojqrwaR6bZo9sU",
+  authDomain: "crud-6671c.firebaseapp.com",
+  projectId: "crud-6671c",
+  storageBucket: "crud-6671c.appspot.com",
+  messagingSenderId: "1067770999919",
+  appId: "1:1067770999919:web:ede3a4873f64ae4f7c099a",
+};
+const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app); // Initialize Firestore
